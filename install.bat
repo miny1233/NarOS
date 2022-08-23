ede942a0a76fa0be9501006b671d07f3eb020a5e
miny1233 <1596784863@qq.com>
2022-08-23 14:52:53 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,7 @@
-dd if=boot.bin of=build/xsOS/xsOS.vhd bs=512 count=1
-dd if=setup.bin of=build/xsOS/xsOS.vhd bs=512 count=1 seek=1
+nasm boot/boot.s -o boot/boot.bin
+nasm boot/setup.s -o boot/setup.bin
+nasm kernel/head.s -o kernel/head.bin
+dd if=boot/boot.bin of=build/xsOS/xsOS.vhd bs=512 count=1
+dd if=boot/setup.bin of=build/xsOS/xsOS.vhd bs=512 count=1 seek=1
+dd if=kernel/head.bin of=build/xsOS/xsOS.vhd bs=512 count=1 seek=5
+PAUSE
