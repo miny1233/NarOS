f1985804308b44acc3b4feb0873ab7e733081440
miny1233 <1596784863@qq.com>
2023-04-05 13:20:59 +0800

    转换编码
    GBK转UTF8，要不然老是乱码。虽然说目前的话在Windows下无法编译。但是还是先保留

--- build.bat
+++ build.bat
@@ -1,25 +1,25 @@
 @echo off
 
-echo ±àÒëboot
+echo ç¼–è¯‘boot
 nasm boot/boot.s -o build/boot.bin
 nasm boot/setup.s -o build/setup.bin
 
-echo ±àÒëkernel
+echo ç¼–è¯‘kernel
 set BUILD_FLAG= -m32 -nostartfiles -fno-builtin -nostdinc -fno-pic -fno-pie -nostdlib -fno-stack-protector
 set SOURSE=kernel/head.s device/io.s kernel/main.c
 set REDIRECT= -Ttext 0
 
 gcc %REDIRECT% %BUILD_FLAG% %SOURSE% -o build/kernel.bin
-echo ÖØ¶¨ÏòÄÚ´æ
-echo Õ¹¿ªbss¶Î
+echo é‡å®šå‘å†…å­˜
+echo å±•å¼€bssæ®µ
 objcopy -O binary build/kernel.bin
 
-echo ±àÒëÍê³É£¬ÕıÔÚĞ´Èë
+echo ç¼–è¯‘å®Œæˆï¼Œæ­£åœ¨å†™å…¥
 dd if=build/boot.bin of=build/c.img bs=512 count=1
 dd if=build/setup.bin of=build/c.img bs=512 count=1 seek=1
 dd if=build/kernel.bin of=build/c.img bs=512 count=50 seek=2
 
-echo Çë¼ì²éÊÇ·ñ´æÔÚ´íÎó£¬°´ÏÂEnterºóÆô¶¯ÏµÍ³
+echo è¯·æ£€æŸ¥æ˜¯å¦å­˜åœ¨é”™è¯¯ï¼ŒæŒ‰ä¸‹Enteråå¯åŠ¨ç³»ç»Ÿ
 PAUSE
 cd build
 bochs -q
