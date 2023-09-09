dfdc3b51a2135bf79eb61812f4cce15935c81d6b
miny1233 <1596784863@qq.com>
2023-09-09 15:17:52 +0800

    先写一点点vfs
    关于vfs的设计上，准备依赖于exfat文件系统了，使用system的定义来标记块设备

--- include/nar/vfs.h
+++ include/nar/vfs.h
@@ -5,6 +5,14 @@
 #ifndef NAROS_VFS_H
 #define NAROS_VFS_H
 
+#include "../../kernel/fs/fat.h"
+
+typedef int fd_t;   //文件描述
+
+#define N_FILE AM_ARC           //普通文件
+#define N_SYS (AM_SYS | AM_ARC) //系统文件（受保护的）
+#define N_DEV AM_SYS            //NAR拓展类
+
 //虚拟文件系统启动并安装根文件系统
 void vfs_init();
 
--- kernel/fs/vfs.c
+++ kernel/fs/vfs.c
@@ -6,21 +6,20 @@
 #include <nar/printk.h>
 #include "fat.h"
 
-FATFS rootfs;
+FATFS rootfs; //根文件系统内存
 
 void vfs_init()
 {
     printk("rootfs addr: 0x%X\n",&rootfs);
     int res = f_mount(&rootfs,"0:/",0);
     if(res == 0)printk("mount rootfs success\n");
-    //printk("set label res %d",res);
+
     DIR dir;
     FILINFO fno;
     int nfile, ndir;
     //f_mkdir("/dev/");
-    FIL fp;
-    res = f_open(&fp,"/nar",FA_READ);
-    printk("open file %d\n",res);
+    f_chmod("/nar", AM_SYS ,0xff);
+
 
     res = f_opendir(&dir, "/");                       /* Open the directory */
     if (res == FR_OK) {
@@ -29,10 +28,10 @@ void vfs_init()
             res = f_readdir(&dir, &fno);                   /* Read a directory item */
             if (res != FR_OK || fno.fname[0] == 0) break;  /* Error or end of dir */
             if (fno.fattrib & AM_DIR) {            /* Directory */
-                printk("<DIR>   %s\n", fno.fname);
+                printk("<DIR>   %s attr: %x\n", fno.fname,fno.fattrib);
                 ndir++;
             } else {                               /* File */
-                printk("<FILE>  %d %s\n",(unsigned int)fno.fsize,fno.fname);
+                printk("<FILE>  %d %s attr: %x\n",(unsigned int)fno.fsize,fno.fname,fno.fattrib);
                 nfile++;
             }
         }
