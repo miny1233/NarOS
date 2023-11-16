c3593324d1e1f91fe4bf80c89306ad661af567f8
miny1233 <1596784863@qq.com>
2023-11-16 22:59:11 +0800

    修改函数设计

--- kernel/task.c
+++ kernel/task.c
@@ -15,6 +15,19 @@ extern tss_t tss;
 
 extern void interrupt_handler_ret_0x20();  //时钟中断返回地址
 
+// 初始化中断栈
+static void stack_init(void* entry,void* stack_top)
+{
+    interrupt_stack_frame* stack = stack_top - sizeof(interrupt_stack_frame);
+    stack->ret = (u32)interrupt_handler_ret_0x20;  //需要使用取地址符号 外部声明是u32函数会被当作变量
+    stack->vector = 0x20;
+    stack->ebp = (u32)stack_top;
+    stack->esp = (u32)&stack->eip;    //IA32中此值被忽略
+    stack->eip = (u32)entry;
+    stack->cs = KERNEL_CODE_SELECTOR;
+    stack->eflags=582;
+}
+
 static void clock_int(int vector)
 {
     assert(vector == 0x20);
@@ -27,12 +40,15 @@ void schedule()
 {
     //寻找下一个任务
     volatile task_t *back_task = running;
+
     next_task:
     running = running->next;
-    if(process_num > 1 && running->pid == 0)goto next_task;
+    if(process_num > 1 && running->pid == 0)
+        goto next_task;
 
     // 如果中断涉及DPL改变 会使用ss0 esp0替换
-    if(running->dpl != 0)tss.esp0 = (u32)running->kernel_stack + PAGE_SIZE;
+    if(running->dpl != 0)
+        tss.esp0 = (u32)running->kernel_stack + PAGE_SIZE;
 
     // 是否需要切换页目录
     if(get_cr3() != running->cr3)
@@ -41,7 +57,6 @@ void schedule()
 
     context_switch(back_task, running);
 }
-
 
 // 以下是初始化过程
 void task_init()
@@ -68,21 +83,16 @@ void task_init()
     set_interrupt_mask(0,1);    // 允许时钟中断
 
 }
-// 初始化中断栈
-static void stack_init(void* entry,void* stack_top)
+
+pid_t kernel_clone()
 {
-    interrupt_stack_frame* stack = stack_top - sizeof(interrupt_stack_frame);
-    stack->ret = (u32)interrupt_handler_ret_0x20;  //需要使用取地址符号 外部声明是u32函数会被当作变量
-    stack->vector = 0x20;
-    stack->ebp = (u32)stack_top;
-    stack->esp = (u32)&stack->eip;    //IA32中此值被忽略
-    stack->eip = (u32)entry;
-    stack->cs = KERNEL_CODE_SELECTOR;
-    stack->eflags=582;
+    return -EINVAL;
 }
+
 
 // 创建内核任务 需要提供程序入口
 task_t* task_create(void *entry) {
+
     if(process_num > MAX_TASK_NUM)
         goto fail;
 
@@ -93,24 +103,30 @@ task_t* task_create(void *entry) {
     stack_init(entry,stack_top);
 
     u32 task_idx;
+    pcb_t* new_task;
 
     for(task_idx=1;task_idx < MAX_TASK_NUM;task_idx++)
     {
         if(task_list[task_idx].pid == 0)    //无任务
+        {
+            new_task = &task_list[task_idx];
             goto create_task;
+        }
     }
     goto fail;
 
     create_task:
     // 设置进程信息
-    task_list[task_idx].pid = ++pid_total;
-    task_list[task_idx].next = running->next;
-    task_list[task_idx].esp = (u32)stack_top - sizeof(interrupt_stack_frame);
-    task_list[task_idx].ebp = (u32)stack_top;
-    task_list[task_idx].cr3 = get_cr3();    //与主进程共享cr3
-    task_list[task_idx].dpl = 0;    // 内核态
-    task_list[task_idx].kernel_stack = get_page(); //陷入内核态时堆栈
-    running->next = &task_list[task_idx];
+    new_task->pid = ++pid_total;
+    new_task->next = running->next;
+    new_task->esp = (u32)stack_top - sizeof(interrupt_stack_frame);
+    new_task->ebp = (u32)stack_top;
+    new_task->cr3 = get_cr3();    //与主进程共享cr3
+    new_task->dpl = 0;    // 内核态
+    new_task->kernel_stack = get_page(); //陷入内核态时堆栈
+
+    //下一个任务指向新任务
+    running->next = new_task;
     process_num++;  //运行任务数+1
 
     set_interrupt_state(1); //  任务创建完毕
@@ -149,6 +165,7 @@ pid_t create_user_mode_task(void* entry)
     void* stack_top = get_page() + PAGE_SIZE;  // 栈顶
     // ROP技术
     interrupt_stack_frame* stack = stack_top - sizeof(interrupt_stack_frame);
+
     stack->ret = (u32)interrupt_handler_ret_0x20;  //需要使用取地址符号 外部声明是u32函数会被当作变量
     stack->vector = 0x20;
     stack->ebp = (u32)stack_top;
@@ -169,25 +186,28 @@ pid_t create_user_mode_task(void* entry)
     stack->eflags=(0 << 12 | 0b10 | 1 << 9);
 
     u32 task_idx;
+    pcb_t* new_task;
     //寻找空的pcb位置
     for(task_idx = 1;task_idx < MAX_TASK_NUM;task_idx++)
-        if(task_list[task_idx].pid == 0)
+        if(task_list[task_idx].pid == 0) {
+            new_task = &task_list[task_idx];
             goto set_process;//无任务
+        }
 
     fail:
     return -1;
 
     set_process:
     // 设置进程信息
-    task_list[task_idx].pid = ++pid_total;
-    task_list[task_idx].next = running->next;
-    task_list[task_idx].esp = (u32)stack_top - sizeof(interrupt_stack_frame);
-    task_list[task_idx].ebp = (u32)stack_top;
+    new_task->pid = ++pid_total;
+    new_task->next = running->next;
+    new_task->esp = (u32)stack_top - sizeof(interrupt_stack_frame);
+    new_task->ebp = (u32)stack_top;
     //现在是测试，应该复制页表
-    task_list[task_idx].cr3 = get_cr3();
-    task_list[task_idx].dpl = 3;    //用户态
+    new_task->cr3 = get_cr3();
+    new_task->dpl = 3;    //用户态
 
-    running->next = &task_list[task_idx];
+    running->next =new_task;
     process_num++;  //运行任务数+1
 
     set_interrupt_state(1); //  任务创建完毕
